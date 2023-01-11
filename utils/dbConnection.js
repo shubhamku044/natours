c35ad12752c1193e0e576d4bf28315958f39d0a1
Shubham Sharma <shubhamku044@gmail.com>
2023-01-11 16:08:42 +0530

    feat: database connection

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose');
 
 const connectDB = async dbUrl => {
   try {
-    console.log(dbUrl);
     mongoose.set('strictQuery', true);
-    await mongoose.connect(dbUrl, () => {
-      console.log('Database connected sucessfully✅');
-    });
+    await mongoose.connect(
+      dbUrl,
+      {
+        dbName: 'natours'
+      },
+      () => {
+        console.log('Database connected sucessfully✅');
+      }
+    );
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
   }
 };
 
